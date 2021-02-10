175521baca9baf076ddb83978486cecda19101fb
Mihir Jain <67909784+mihirjain-iitgn@users.noreply.github.com>
2021-02-10 23:39:04 +0530

    Add files via upload

--- clean.py
+++ clean.py
@@ -16,23 +16,23 @@ n1 = len(pop)
 n2 = len(le)
 n3 = len(cm)
 
-pd.read_csv("./finaldata.csv")
+# pd.read_csv("./finaldata.csv")
 
-# fp = open("./finaldata.csv","w")
+fp = open("./finaldata.csv","w")
 
-# for i in range(n1):
-#     country1 = pop.loc[i,"country"]
-#     for j in range(n2):
-#         country2 = le.loc[j,"country"]
-#         if (country1==country2):
-#             for k in range(n3):
-#                 country3 = cm.loc[k,"country"]
-#                 if (country2 == country3):
-#                     for years in range(1990,2021):
-#                         years_ = str(years)
-#                         country = modified(country1)
-#                         x = str(pop.loc[i,years_])
-#                         y = str(le.loc[j,years_])
-#                         z = str(cm.loc[k,years_])
-#                         fp.write(x+","+y+","+z+","+years_+","+country+"\n")
-# fp.close()
+for i in range(n1):
+    country1 = pop.loc[i,"country"]
+    for j in range(n2):
+        country2 = le.loc[j,"country"]
+        if (country1==country2):
+            for k in range(n3):
+                country3 = cm.loc[k,"country"]
+                if (country2 == country3):
+                    for years in range(1990,2021):
+                        years_ = str(years)
+                        country = modified(country1)
+                        x = str(pop.loc[i,years_])
+                        y = str(le.loc[j,years_])
+                        z = str(cm.loc[k,years_])
+                        fp.write(x+","+y+","+z+","+years_+","+country+"\n")
+fp.close()
